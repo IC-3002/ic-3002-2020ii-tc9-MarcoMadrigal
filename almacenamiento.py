568c10ef89f1db49f07c02eb2e684c39250841cf
Marco Madrigal <MadrigalMarco>
2020-12-01 22:47:06 -0600

    Tarea lista

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,2 +1,21 @@
 def maximizar(As, D):
-    raise NotImplementedError()
+
+    lista_ordenada = sorted(As, key=segundo)
+    peso = 0
+    M = []
+
+    for x in range(len(As)):
+        
+        if peso+lista_ordenada[x][1] <=D:
+            peso += lista_ordenada[x][1]
+            M.append(lista_ordenada[x])
+
+        else:
+            break
+
+    return M
+
+
+def segundo(elemento):
+
+    return elemento[1]
